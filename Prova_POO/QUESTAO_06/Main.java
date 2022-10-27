f8a1683f3c85b2cce281df936462f913e1531836
404notfound-max <60528618+404notfound-max@users.noreply.github.com>
2022-10-26 22:25:54 -0300

    Update Main.java

--- Prova_POO/QUESTAO_06/Main.java
+++ Prova_POO/QUESTAO_06/Main.java
@@ -1,24 +1,28 @@
-package POO.Prova_POO.QUESTAO_06;
+package ProvaQ06;
 
 import java.util.ArrayList;
 
-public class Main {
-    public static void main(String[] args) {
-        ArrayList<Conta_corrente> contas = new ArrayList<>();
-        contas.add(new Conta_corrente(500));
-        contas.add(new Conta_especial(500, 300));
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        ArrayList<ContaCorrente> contas = new ArrayList<>();
+        contas.add(new ContaCorrente(500));
+        contas.add(new ContaEspecial(500, 300));
 
         Cliente cliente = new Cliente("Joaquim", contas);
-        Conta_corrente conta_corrente = cliente.getContas().get(0);
+        ContaCorrente contaCorrente = cliente.getContas().get(0);
 
-        if(conta_corrente.sacar(100)) {
+        if(ContaCorrente.Sacar(100)) 
+        {
             System.out.println("Saquei 100 reais da conta corrente");
         }
 
-        Conta_corrente conta_especial = cliente.getContas().get(1);
-        if(conta_especial.sacar(800)) {
+        ContaCorrente contaEspecial = cliente.getContas().get(1);
+        if(ContaEspecial.Sacar(800)) 
+        {
             System.out.println("Sacando 800 reais da conta especial");
         }
-        System.out.println(conta_especial.getSaldo());
+        System.out.println(contaEspecial.getSaldo());
     } 
 }
